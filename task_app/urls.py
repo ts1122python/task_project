fa12c516ff1b33aec5c5d31fda03df4b6a730c48
Tsukuni <1122pyweb.tsukuni@gmail.com>
2025-03-11 13:34:42 +0900

    3月11日学校分_最新

--- task_app/urls.py
+++ task_app/urls.py
@@ -6,12 +6,12 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('schedule/', views.schedule, name='schedule'),
     path('create_task/', views.create_task, name='create_task'),
-    path('task/edit/<int:num>/', views.edit_task, name='edit_task'),
+    path('edit_task/', views.edit_task, name='edit_task'),
     path('task_list/', views.task_list, name='task_list'),
+    path('detail_task/<int:item.id>', views.detail_task, name='detail_task'),
 ]
 
 #
-#    path('detail_task/', detail_task, name='detail_task'),
 
 #    path('user_list/', user_list, name='user_list'),
 #    path('profile/', profile, name='profile'),
