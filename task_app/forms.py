ce968d31129e3603164d37599ed93eb0de5ad2f2
Tsukuni <1122pyweb.tsukuni@gmail.com>
2025-03-13 14:16:32 +0900

    3月13日分

--- task_app/forms.py
+++ task_app/forms.py
@@ -1,25 +1,46 @@
 from django import forms
 from .models import CustomUser,Task,Comment
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.forms import UserCreationForm
-
-# 新規登録のフォーム
-class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = ('username','department', )
-
-    def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-
-
-# CustomUserのフォーム
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 # 日本語の月名リスト（1月～12月）
 MONTHS_JP = {
     1: "1月", 2: "2月", 3: "3月", 4: "4月", 5: "5月", 6: "6月",
     7: "7月", 8: "8月", 9: "9月", 10: "10月", 11: "11月", 12: "12月"
 }
+
+
+# CustomUserのフォーム
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = "username", "department", "my_goal" # passwordは設定しない
+        widgets = {
+            'username':forms.TextInput(attrs={'class':'form-control'}),
+            'department':forms.TextInput(attrs={'class':'form-control'}),
+            'my_goal':forms.TextInput(attrs={'class':'form-control'}),
+        }
+        labels = {
+            "username": "名前",
+            "department": "所属",
+            "my_goal": "個人目標",
+        }
+        help_texts = {
+            "username": "名前は必須です。",
+            "department": "所属は必須です。",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = "username", "department", "my_goal" # passwordは設定しない
+        
+
+
 
 # Taskのフォーム
 class TaskForm(forms.ModelForm):
@@ -58,6 +79,12 @@ class TaskForm(forms.ModelForm):
             'work_progress': '進捗',
             'option': '備考',
         }
+        help_texts = {
+            'owner':'担当者名は必須です。',
+            'title': '業務名は必須です。（50字以内）',
+            'work': '業務内容を記載してください。（1,000字以内）',
+        }
+
         
 
 
--- task_app/views.py
+++ task_app/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.core.paginator import Paginator
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from .models import Task
-from .forms import CustomUserCreationForm,TaskForm
+from .models import Task,CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm, TaskForm
 
 
-# ユーザー登録関数
+# ユーザー登録
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
@@ -19,11 +19,13 @@ def register(request):
         form = CustomUserCreationForm()
     return render(request, 'accounts_app/register.html', {'form': form})
 
-# ユーザーログイン関数
+
+# ユーザーログイン
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
         # authenticate関数で認証を試みる
         user = authenticate(request, username=username, password=password)
         
@@ -34,6 +36,60 @@ def user_login(request):
         
     return render(request, 'task_app/login.html')
 
+
+# ユーザーログアウト
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+# ユーザー一覧
+def user_list(request): 
+    users = CustomUser.objects.all() 
+    max = 20 
+    paginator = Paginator(users, max)
+    page_number = int(request.GET.get('page', 1)) 
+    page_obj = paginator.get_page(page_number) 
+    
+    params = {
+        'title' : '職員一覧' ,
+        'users' : page_obj ,
+    }
+    return render(request, 'task_app/user_list.html', params)
+
+
+# 自分以外のプロフィール表示
+def profile(request, user_id):
+    user_data = get_object_or_404(CustomUser, id=user_id)
+    params = {
+        'title' : 'プロフィール' ,
+        'user_data' : user_data ,
+        'user_id' : user_id ,
+    }
+    return render(request,'task_app/profile.html', params)
+
+
+# 自分のプロフィールを表示
+@login_required(login_url='/task_app/login')
+def my_profile(request):
+    my_data = request.user  # ログインユーザーの情報を取得
+    return render(request, 'task_app/my_profile.html', {'my_data': my_data})
+
+
+# 自分のプロフィール修正
+@login_required(login_url='/task_app/login')
+def edit_profile(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('my_profile')) 
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    return render(request, 'task_app/edit_profile.html', {'form': form })
+
+
+
 # スケジュール関数
 def schedule(request):
     return render(request, 'task_app/schedule.html')
@@ -42,36 +98,32 @@ def schedule(request):
 def create_task(request):
     if request.method == 'POST':
         obj = Task()
-        task = TaskForm(request.POST, instance=obj)
-        task.save()
-        return redirect(reverse('detail_task',kwargs={'task_id': task_id}))
+        task_form = TaskForm(request.POST, instance=obj)
+        task_form.save()
+        return redirect(reverse('detail_task',kwargs={'task_id': obj.id}))
     
     params = {
         'title':'業務追加',
-        'form': TaskForm(),
+        'task_form': TaskForm(),
     }
     
     return render(request, 'task_app/create_task.html', params)
 
 # タスク編集関数
 def edit_task(request,task_id): # リクエストを受けて因数をtask_idとして受け取る関数
-    obj = get_object_or_404(Task, id=task_id)
-        # objにTaskモデルに存在するidがtask_idのインスタンスをgetして代入
+    obj = get_object_or_404(Task, id=task_id) # objにTaskモデルに存在するidがtask_idのインスタンスをgetして代入
     if request.method == 'POST':
-        task_form = TaskForm(request.POST, instance=obj) 
-            # taskにrequest.POSTを受けた場合、インスタンスに編集されたobjを代入
+        task_form = TaskForm(request.POST, instance=obj)  # taskにrequest.POSTを受けた場合、インスタンスに編集されたobjを代入
         if task_form.is_valid():  # フォームが正しい場合のみ保存
-            task_form.save() 
-                #task_formの内容をデータベースに登録
-            return redirect(reverse('detail_task',kwargs={'task_id': task_id}))
-                # detail_taskにtask_idを引数にとってリダイレクトする
+            task_form.save()  #task_formの内容をデータベースに登録
+            return redirect(reverse('detail_task',kwargs={'task_id': task_id})) # detail_taskにtask_idを引数にとってリダイレクトする
     else:
         task_form = TaskForm(instance=obj)
     
     params = {
-        'title': 'タスク編集' ,
-        'id': task_id ,
-        'form': task_form ,
+        'title': '業務編集' ,
+        'task_id': task_id ,
+        'task_form': task_form ,
     }
     return render(request, 'task_app/edit_task.html', params)
 
@@ -85,15 +137,15 @@ def delete_task(request, task_id):
 def detail_task(request, task_id):
     task_data = get_object_or_404(Task, id=task_id)
     params = {
-        'title' : 'タスク詳細' ,
-        'data' : task_data ,
+        'title' : '業務詳細' ,
+        'task' : task_data ,
         'id' : task_id
     }
     return render(request,'task_app/detail_task.html', params)
 
 # タスク一覧
 @login_required(login_url='/task_app/login')
-def task_list(request): # 初期設定のページは１ページ目
+def task_list(request):
     tasks = Task.objects.all() # 全てのタスクを取得
     max = 5 # 1ページあたりの表示数
     paginator = Paginator(tasks, max) # ページネーションでページを取得
@@ -101,22 +153,13 @@ def task_list(request): # 初期設定のページは１ページ目
     page_obj = paginator.get_page(page_number) # get_page(page) を呼ぶと、page目のPageインスタンスを取得します。
     
     params = {
-        'title' : 'タスク一覧' ,
+        'title' : '業務一覧' ,
         'tasks' : page_obj ,
     }
     return render(request, 'task_app/task_list.html', params)
 
-
-# ユーザー一覧
-#@login_required(login_url='/task_app/login')
-
-# プロフィール
-def my_profile(request):
-    pass
-
-
 # 成果
 
-# プロフィール修正
+
 
 #render関数...render(request, template_name（描画するテンプレートの名前）, context（テンプレートに渡すデータを含む辞書）
